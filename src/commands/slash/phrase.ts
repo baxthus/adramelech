64b93ccddb21b9f16c7eead543dbb897f28e18fa
baxthus <baxt@baxt.fun>
2025-05-17 22:09:08 -0300

    feat: add phrase command
    Closes KLAUTZ-102

---
+++ src/commands/slash/phrase.ts
@@ -0,0 +1,35 @@
+import { ComponentType, MessageFlags, SlashCommandBuilder } from 'discord.js';
+import env from '~/env';
+import type { Command } from '~/types/command';
+import loadPhrases from '~/utils/loadPhrases';
+
+const phrases = await loadPhrases();
+
+export const command = <Command>{
+  data: new SlashCommandBuilder()
+    .setName('phrase')
+    .setDescription('Get a random phrase'),
+  async execute(interaction) {
+    const phrase = phrases[Math.floor(Math.random() * phrases.length)];
+
+    await interaction.reply({
+      flags: MessageFlags.IsComponentsV2,
+      components: [
+        {
+          type: ComponentType.Container,
+          accent_color: env.EMBED_COLOR,
+          components: [
+            {
+              type: ComponentType.TextDisplay,
+              content: `\`\`\`${phrase.content}\`\`\``,
+            },
+            {
+              type: ComponentType.TextDisplay,
+              content: `> ${phrase.source}`,
+            },
+          ],
+        },
+      ],
+    });
+  },
+};
