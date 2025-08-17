beeb954d117920e211504c1f7d041fdf4c2991df
baxthus <baxt@baxt.fun>
2025-08-16 21:16:56 -0300

    feat: setup shadcn-svelte

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import '@fontsource/geist-sans';
+	import '@fontsource/boldonse';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<title>Adramelech Dashboard</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<ModeWatcher />
+
+<main class="p-4">
+	{@render children?.()}
+</main>
