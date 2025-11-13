73068eb5bcf5b5016f7737fd4df911ce117bb0c6
baxthus <baxt@baxt.dev>
2025-11-13 16:09:24 -0300

    feat(dashboard-next): add Sign In button to Home page

--- dashboard-next/src/app/page.tsx
+++ dashboard-next/src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-y-8 p-4">
       <h1 className="font-brand text-3xl">Adramelech Dashboard</h1>
+      <Button size="lg" asChild>
+        <Link href="/sign-in">Sign In</Link>
+      </Button>
     </div>
   );
 }
